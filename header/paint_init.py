17ef72a562b990d854d592b73de158364507c5c5
seongjiko <soc06202@naver.com>
2022-05-31 20:57:26 +0900

    source file update

---
+++ header/paint_init.py
@@ -0,0 +1,12 @@
+ADD_COLOR = 0
+REMOVE_COLOR = 1
+CHANGE_COLOR = 2
+RESET_IMAGE = 3
+SHOW_ORIGIN = 4
+EXIT = 5
+
+mouse_mode, draw_mode = 0, 0
+pt1, pt2 = (0, 0), (0, 0)
+Color = (0, 0, 0)
+thickness = 3
+
---
+++ header/paint_utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+def place_icons(image, size):
+    icon_name = ["addIcon", "changeIcon", "originIcon", "removeIcon", "resetIcon", "quitIcon"]
+
+    icons = [(i % 2, i//2, 1, 1) for i in range(len(icon_name))]
+    icons = np.multiply(icons, size*2)
+
+    for roi, name in zip(icons, icon_name):
+        icon = cv2.imread("./images/%s.png" % name, cv2.IMREAD_COLOR)
+        if icon is None:
+            print("Can't load img")
+            continue
+
+        else:
+            print(f"success load {name} img file.")
+
+        x, y, w, h = roi
+        image[y:y+h, x:x+w] = cv2.resize(icon, size)
+    return list(icons)
+
+def create_hueIndex(image, roi):
+    x, y, w, h = roi
+    index = [[(j, 1, 1) for j in range(w)] for i in range(h)]
+    ratios = (180/w, 255, 255)
+    hueIndex = np.multiply(index, ratios).astype('uint8')
+    image[y:y+h, x:x+w] = cv2.cvtColor(hueIndex, cv2.COLOR_HSV2BGR)
+
+def create_colorPlatte(image, idx, roi):
+    x, y, w, h = roi
+    hue = idx-x
+    palette = [[(hue, j, h-i-1) for j in range(w)] for i in range(h)]
+
+    ratios = (180/w, 255/w, 255/h)
+    palette = np.multiply(palette, ratios).astype('uint8')
+
+    image[y:y+h, x:x+w] = cv2.cvtColor(palette, cv2.COLOR_HSV2BGR)
+
+# image = np.full((500, 800, 3), 255, np.uint8)
+# icons = place_icons(image, (60, 60))
+# cv2.imshow("PaintCV", image)
+# cv2.waitKey(0)
